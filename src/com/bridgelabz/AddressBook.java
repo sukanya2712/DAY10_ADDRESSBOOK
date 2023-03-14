d0c703ca1dae76efaa856a9e555149d9387e4d4a
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-14 23:41:34 +0530

    UC2_ADDED

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -3,5 +3,37 @@ package com.bridgelabz;
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to addressbook");
+        Contact contact = new Contact();
+        public void addContact() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the values of contact");
+            System.out.println("Enter the First Name");
+            String firstName = scanner.next();
+            System.out.println("Enter the Last Name");
+            String lastName = scanner.next();
+            System.out.println("Enter the Address");
+            String address = scanner.next();
+            System.out.println("Enter the City");
+            String city = scanner.next();
+            System.out.println("Enter the State");
+            String state = scanner.next();
+            System.out.println("Enter the zip");
+            String zip = scanner.next();
+            System.out.println("Enter the Phone Number");
+            String phoneNumber = scanner.next();
+            contact.setFirstName(firstName);
+            contact.setLastName(lastName);
+            contact.setAddress(address);
+            contact.setCity(city);
+            contact.setState(state);
+            contact.setZip(zip);
+            contact.setPhoneNumber(phoneNumber);
+        }
+        @Override
+        public String toString() {
+            return "AddressBook{" +
+                    "contact=" + contact +
+                    '}';
+        }
     }
 }
---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
+        System.out.println(addressBook);
+    }
+}
