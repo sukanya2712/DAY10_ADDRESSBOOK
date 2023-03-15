7e29b3fd2385b2c5cd17456cbd2a36a508fd9235
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-16 01:03:56 +0530

    ADDED_uc1

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to addressbook");
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            Contact c= new Contact();
+            System.out.print("Enter First Name: ");
+            c.setFirstName(sc.next());
+            System.out.print("Enter Last Name: ");
+            c.setLastName(sc.next());
+            System.out.print("Enter State: ");
+            c.setState(sc.next());
+
+            System.out.println(c.getFirstName());
+            System.out.println(c.getLastName());
+            System.out.println(c.getState());
     }
 }
