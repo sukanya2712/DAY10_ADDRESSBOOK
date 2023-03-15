d8686e3451d4351da0f65c3a0bb0e14bbc701785
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-16 00:45:22 +0530

    Added_UC1

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,39 +1,21 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBook {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to addressbook");
-        Contact contact = new Contact();
-        public void addContact() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter the values of contact");
-            System.out.println("Enter the First Name");
-            String firstName = scanner.next();
-            System.out.println("Enter the Last Name");
-            String lastName = scanner.next();
-            System.out.println("Enter the Address");
-            String address = scanner.next();
-            System.out.println("Enter the City");
-            String city = scanner.next();
-            System.out.println("Enter the State");
-            String state = scanner.next();
-            System.out.println("Enter the zip");
-            String zip = scanner.next();
-            System.out.println("Enter the Phone Number");
-            String phoneNumber = scanner.next();
-            contact.setFirstName(firstName);
-            contact.setLastName(lastName);
-            contact.setAddress(address);
-            contact.setCity(city);
-            contact.setState(state);
-            contact.setZip(zip);
-            contact.setPhoneNumber(phoneNumber);
-        }
-        @Override
-        public String toString() {
-            return "AddressBook{" +
-                    "contact=" + contact +
-                    '}';
-        }
+        Scanner sc = new Scanner(System.in);
+        Contact c= new Contact();
+        System.out.print("Enter First Name: ");
+        c.setFirstName(sc.next());
+        System.out.print("Enter Last Name: ");
+        c.setLastName(sc.next());
+        System.out.print("Enter State: ");
+       c.setState(sc.next());
+
+        System.out.println(c.getFirstName());
+        System.out.println(c.getLastName());
+        System.out.println(c.getState());
     }
 }
--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -3,27 +3,7 @@ package com.bridgelabz;
 public class Contact {
     private String firstName;
     private String lastName;
-    private String address;
     private String state;
-    private String city;
-    private String email;
-    private String zip;
-    private String phoneNumber;
-
-
-    public Contact(String firstName, String lastName, String address, String state, String city, String email, String zip, String phonenumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.state = state;
-        this.city = city;
-        this.email = email;
-        this.zip = zip;
-        this.phoneNumber = phonenumber;
-    }
-
-    public Contact() {
-    }
 
     public String getFirstName() {
         return firstName;
@@ -41,14 +21,6 @@ public class Contact {
         this.lastName = lastName;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getState() {
         return state;
     }
@@ -57,49 +29,4 @@ public class Contact {
         this.state = state;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", state='" + state + '\'' +
-                ", city='" + city + '\'' +
-                ", email='" + email + '\'' +
-                ", zip='" + zip + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                '}';
-    }
 }
