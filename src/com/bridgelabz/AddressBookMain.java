247c1c1da1134da1ca85c23152a2484607f21174
sukanya naik <95672385+sukanya2712@users.noreply.github.com>
2023-03-16 00:32:45 +0530

    Delete AddressBookMain.java

--- src/com/bridgelabz/AddressBookMain.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz;
-
-public class AddressBookMain {
-    public static void main(String[] args) {
-        AddressBook addressBook = new AddressBook();
-        addressBook.addContact();
-        System.out.println(addressBook);
-    }
-}
